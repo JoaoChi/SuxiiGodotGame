5508dd445189458a9c6578ca97f0d80b449cc5ed
João Ampeze <joao.ampeze@cordenonsi.com.br>
2026-02-19 20:37:21 -0300

    Feat/ Adição do fundo mainmenu e menubuttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,4 +1,10 @@
 extends Control
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+func _on_novo_jogo_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_configuracao_pressed():
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
+
+func _on_sair_pressed():
+	get_tree().quit()
