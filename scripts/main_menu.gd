ca41195489f295360454bccaa21888b3f4bfc18f
João Ampeze <joao.ampeze@cordenonsi.com.br>
2026-02-20 11:35:59 -0300

    Feat/ Animated - menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,23 @@
 extends Control
 
-func _on_novo_jogo_pressed():
+@onready var video_player: VideoStreamPlayer = $ContinuarVideo
+@onready var menu_panel: PanelContainer = $PanelContainer
+
+func _on_continuar_pressed() -> void:
+	menu_panel.visible = false
+	video_player.visible = true
+	video_player.play()
+
+func _on_video_finished() -> void:
+	video_player.visible = false
+	menu_panel.visible = true
+	get_tree().change_scene_to_file("res://scenes/atendente.tscn")
+
+func _on_novo_jogo_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-func _on_configuracao_pressed():
+func _on_configuracao_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
-func _on_sair_pressed():
+func _on_sair_pressed() -> void:
 	get_tree().quit()
